681a51637768af25648a56977d67e42b9ca04829
Reginaldo Junior <reginaldo.junior696@gmail.com>
2023-01-21 16:10:54 -0300

    added pins models and migrations

---
+++ app/Http/Controllers/PinController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pin;
+use Illuminate\Http\Request;
+
+class PinController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Pin  $pin
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pin $pin)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Pin  $pin
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pin $pin)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Pin  $pin
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pin $pin)
+    {
+        //
+    }
+}
